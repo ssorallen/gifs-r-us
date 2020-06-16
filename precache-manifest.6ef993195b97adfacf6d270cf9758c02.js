be0d9e9476deaab0332407949691f61cba3e655f
Ross Allen <rallen@boweryfarming.com>
2020-06-15 22:17:25 -0500

    Updates

---
+++ precache-manifest.6ef993195b97adfacf6d270cf9758c02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb87b2f91f2c34d37814fc83239744c9",
+    "url": "/gifs-r-us/index.html"
+  },
+  {
+    "revision": "ed1b762c970c42fcbe8a",
+    "url": "/gifs-r-us/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5d158a28fe013a7ddd2a",
+    "url": "/gifs-r-us/static/css/main.be1b8718.chunk.css"
+  },
+  {
+    "revision": "ed1b762c970c42fcbe8a",
+    "url": "/gifs-r-us/static/js/2.6509a368.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gifs-r-us/static/js/2.6509a368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d158a28fe013a7ddd2a",
+    "url": "/gifs-r-us/static/js/main.58167257.chunk.js"
+  },
+  {
+    "revision": "3e52fb94eb2ded06b651",
+    "url": "/gifs-r-us/static/js/runtime-main.2347d797.js"
+  }
+]);
